83f88420a786461868735ae3646a0090d3fe0126
German Alamilla Peralta <german.alamilla@gmail.com>
2021-01-04 10:41:27 -0600

    database setup and header

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from "react";
+import './App.css';
+import Header from './Header';
+import Home from './Home';
+
+function App() {
+  return (
+    //BEM
+    <div className="app">
+        {/* Header */}
+        <Header />
+        {/* Home */}
+        <Home />
+        {/* */}
+
+    </div>
+  );
+}
+
+export default App;
---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyCJmwaCMVJtkBH44dRY02ctTFCSSHPEUKQ",
+    authDomain: "a-1bf14.firebaseapp.com",
+    projectId: "a-1bf14",
+    storageBucket: "a-1bf14.appspot.com",
+    messagingSenderId: "433115915409",
+    appId: "1:433115915409:web:5bfd4e8d1e8f3c0c4c8b30",
+    measurementId: "G-KTNRCH647V"
+  };
